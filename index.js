ca5f8104bbcaa16e81685dca3f725fb09ec6edf6
Mobolanle Adebesin <53238865+MobolanleAdebesin@users.noreply.github.com>
2019-08-08 21:56:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ var randomNum = Math.floor(Math.random() * 6 + 1);
 var randomImg = "dice" + randomNum +".png"; 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImg);
-
+//Die 2 
+var randomNum2 = Math.floor(Math.random() * 6 + 1); 
+var randomImg2 = "dice" + randomNum2 + ".png";
+var image2 = document.querySelectorAll("img")[1]; 
+image2.setAttribute("src", randomImg2); 
