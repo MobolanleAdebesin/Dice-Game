f5dfd814e53af4b42fc45539ea01e1ebedfc0428
Mobolanle Adebesin <53238865+MobolanleAdebesin@users.noreply.github.com>
2019-08-08 22:12:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ var randomNum2 = Math.floor(Math.random() * 6 + 1);
 var randomImg2 = "dice" + randomNum2 + ".png";
 var image2 = document.querySelectorAll("img")[1]; 
 image2.setAttribute("src", randomImg2); 
+//Who Wins 
+var header = document.querySelector("h1"); 
+if(randomNum > randomNum2){
+  header.innerHTML = "⚑ Player 1 Wins! ⚑"; 
+}
+else if(randomNum < randomNum2){
+  header.innerHTML = "⚑ Player 2 Wins! ⚑";
+}
+else{
+  header.innerHTML = "It's a tie!"; 
+}
+
